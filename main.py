c32a37855789b305cdffcfe42e4b9da3a978a36a
eduxrdx <edugit88@gmail.com>
2023-05-09 18:59:57 +0100

    AES CTR mode implementation

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from sys import exit
+
 import encryption_functions as ef
 import file_management as fm
 import mac_functions as mf
 import print_info as pi
+import hash_functions as hf
 
 
 # GLOBAL VARIABLES
@@ -20,30 +23,24 @@ def main():
             secret_key = input("Password: ").lower()
             message = input("Message: ")
 
-            # TODO: hmac needs to be changed, as it receives the ciphertext and not the plaintext message
-            # Attention to the fact that it is more secure to encrypt the message first and then calculate the hmac,
-            # lastly concatenate the hmac with the ciphertext
-            hmac_value = mf.hmac_sender(message, secret_key.encode("utf-8"))
-            iter_counter, salt, ciphertext_iv = ef.encrypt_message(message, secret_key)
-            fm.write_file(iter_counter, question, salt, ciphertext_iv, hmac_value)
+            iter_counter, salt, ciphertext = ef.encrypt_message(message, secret_key)
+            hmac_value = mf.calculate_hmac(ciphertext, secret_key)
+            print(f"HMAC: {hmac_value}")
+            fm.write_file(iter_counter, question, salt, ciphertext, hmac_value)
 
         elif option == "2":
             ciphertext = fm.read_file(FILE_NAME)
             question = ciphertext[1]
 
             print(f"Question: {question}")
-            secret_key = input("Password: ")
+            secret_key = input("Password: ").lower()
             
-            decrypted_message, hmac_received = ef.decrypt_message(secret_key, ciphertext)
+            decrypted_message, hmac_validity = ef.decrypt_message(secret_key, ciphertext)
 
-            if decrypted_message is None:
-                print("Wrong password. Try again!")
-                continue
+            print(f"hmac: {hmac_validity}")
 
-            if mf.hmac_receiver(decrypted_message, secret_key.encode("utf-8"), hmac_received):
-                print("HMAC is valid")
-            else:
-                print("HMAC is invalid")
+        elif option == "0":
+            exit(0)
 
 
 if __name__ == "__main__":
