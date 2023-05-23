d629b230a7324ea01c2e735a83123a07d7d2b4f5
eduxrdx <edugit88@gmail.com>
2023-05-23 19:57:05 +0100

    Added function descriptions

--- database_management.py
+++ database_management.py
@@ -5,16 +5,19 @@ import bcrypt
 DATABASE_PATH = 'database.json'
 
 
+# Hashes the password inserted by the user
 def password_hashing(password):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     return hashed_password.decode('utf-8')
 
 
+# Checks if the password inserted by the user is the same as the one in the database
 def password_checking(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
+# Creates an empty JSON database
 def create_database():
     empty_database = {
         "id": None,
@@ -26,6 +29,7 @@ def create_database():
         json.dump(empty_database, f)
 
 
+# Adds a new user to the database
 def add_user(username, user_id, password):
     hashed_password = password_hashing(password)
 
@@ -44,6 +48,7 @@ def add_user(username, user_id, password):
         json.dump(database, f)
 
 
+# Checks if the user already exists in the database
 def check_if_user_exists(username):
     with open(DATABASE_PATH, 'r') as f:
         database = json.load(f)
