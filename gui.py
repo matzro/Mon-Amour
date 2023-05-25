46739122ef6209c2f4ca83fd31d01d7354c2a55b
khajiits <matilde.a.rosa@gmail.com>
2023-05-26 00:22:47 +0100

    Decrypted message showing

--- gui.py
+++ gui.py
@@ -8,6 +8,8 @@ import encryption_functions as ef
 import file_management as fm
 import mac_functions as mf
 
+set_appearance_mode('dark')
+set_default_color_theme('blue')
 
 class LoginWindow(CTk):
     def __init__(self, *args, **kwargs):
@@ -124,28 +126,21 @@ class CustomTabView(CTkTabview):
         ))
         self.button_send.grid(row=4, column=1, padx=20, pady=10)
 
+        # Add widgets to Receive tab
         global ciphertext, sender_id
         try:
             ciphertext, sender_id = fm.read_file(username)
             question_received = ciphertext[1]
-            # Add widgets to Receive tab
+
             # Question
-            self.label_question_received = CTkLabel(self.tab("Receive"), text=question_received)
+            self.label_question_received = CTkLabel(self.tab("Receive"), text="Question: " + question_received)
             self.label_question_received.grid(row=0, column=0, padx=20, pady=10)
-            self.entry_question_received = CTkEntry(self.tab("Receive"))
-            self.entry_question_received.grid(row=0, column=1, padx=20, pady=10)
 
             # Answer
             self.label_answer_guess = CTkLabel(self.tab("Receive"), text="Answer")
             self.label_answer_guess.grid(row=1, column=0, padx=20, pady=10)
             self.entry_answer_guess = CTkEntry(self.tab("Receive"))
             self.entry_answer_guess.grid(row=1, column=1, padx=20, pady=10)
-
-            # Message
-            self.label_message_received = CTkLabel(self.tab("Receive"), text="Message")
-            self.label_message_received.grid(row=2, column=0, padx=20, pady=10)
-            self.entry_message_received = CTkEntry(self.tab("Receive"))
-            self.entry_message_received.grid(row=2, column=1, padx=20, pady=10)
 
             # Button
             self.button_receive = CTkButton(self.tab("Receive"), text="Test my love", command=lambda: self.decipher(
@@ -155,6 +150,10 @@ class CustomTabView(CTkTabview):
         except:
             self.label_no_messages = CTkLabel(self.tab("Receive"), text="No messages")
             self.label_no_messages.grid(row=0, column=0, padx=20, pady=10)
+
+        # Message
+        self.label_message_received = CTkLabel(self.tab("Receive"), text="")
+        self.label_message_received.grid(row=2, column=0, padx=20, pady=10)
 
     def cipher(self, username, password, question, secret_key, message, recipient):
         """This function is called when the user clicks on the "Send" button. It reads the values from the GUI - question,
@@ -178,6 +177,14 @@ class CustomTabView(CTkTabview):
         fm.write_file(iter_counter, question, salt, ciphertext_sent, hmac_value, signature, username, recipient)
 
     def decipher(self, secret_key):
+        """This function is called when the user clicks on the "Receive" button. It reads the value from the GUI - secret
+        key.
+        First, the secret key received is decrypted with the private key of the receiver, using RSA.
+        Then, the ciphertext is read from the file and the message is decrypted using AES.
+        The HMAC is calculated and compared with the HMAC received. If they are equal, it is printed "HMAC verified".
+        It is needed to verify the digital signature of the message, so the signature is read from the file and verified. If
+        the signature is valid, it is printed "Signature verified". If not, it is printed "Signature not verified".
+        :return: None"""
 
         sender_username = dbm.get_username_by_id(sender_id)
         signature = ciphertext[4]
@@ -194,6 +201,9 @@ class CustomTabView(CTkTabview):
             print("Signature not verified")
 
         print(f"hmac: {hmac_validity}")
+
+        #Show message
+        self.label_message_received.configure(text=decrypted_message.decode())
 
 
 class MainWindow(CTk):
